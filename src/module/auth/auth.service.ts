ebfa5202d041bd40b3fb9b67d50ecc6ea374bcc8
Mrkwamu <jeffreyfestus1@gmail.com>
2026-05-10 02:37:17 +0100

    feat(auth): add email verification and fix auth race conditions

--- src/module/auth/auth.service.ts
+++ src/module/auth/auth.service.ts
@@ -2,6 +2,7 @@ import {
   BadRequestException,
   ConflictException,
   Injectable,
+  InternalServerErrorException,
   NotFoundException,
   UnauthorizedException,
 } from '@nestjs/common';
@@ -13,6 +14,8 @@ import { ConfigService } from '@nestjs/config';
 import { parseDurationToMs } from '../../common/parse/parse-durarion-to-ms';
 import { JwtPayload, AuthSession, AuthTokenResult } from './types/auth.types';
 import { Prisma } from '../../../generated/prisma/client';
+import { randomInt } from 'crypto';
+import { EmailService } from '../../common/email/email.service';
 
 @Injectable()
 export class AuthService {
@@ -21,6 +24,7 @@ export class AuthService {
     private readonly cryptoService: CryptoService,
     private readonly configService: ConfigService,
     private readonly jwtService: JwtService,
+    private readonly emailService: EmailService,
   ) {}
 
   private generateToken(
@@ -80,48 +84,82 @@ export class AuthService {
     userId: string,
     device: string,
     ipAddress: string,
+    tx?: Prisma.TransactionClient,
   ) {
     const { accessToken, refreshToken, refreshTokenTtlMs, expiresAt } =
       this.createAuthTokens(userId);
 
     const hashedRefreshToken = this.cryptoService.hashValue(refreshToken);
 
-    await this.createSession({
-      userId,
-      token: hashedRefreshToken,
-      device,
-      ipAddress,
-      expiresAt,
-    });
+    await this.createSession(
+      {
+        userId,
+        token: hashedRefreshToken,
+        device,
+        ipAddress,
+        expiresAt,
+      },
+      tx,
+    );
 
     return { accessToken, refreshToken, refreshTokenTtlMs };
   }
 
   async registerUser(dto: RegisterDto, device: string, ipAddress: string) {
     // check if an account with this email already exist
-    const existingUser = await this.prisma.user.findUnique({
-      where: { email: dto.email.toLowerCase() },
-    });
-
-    // return message to tell user there's already an accoount
-    if (existingUser) {
-      throw new ConflictException('Account already exists with this email');
-    }
+    const email = dto.email.toLowerCase().trim();
+    const name = dto.name.trim();
+    const otp = randomInt(100000, 1000000).toString();
+    const expiresAt = Date.now() + 5 * 60 * 1000;
 
     // hash the password before i stored it in the DB
     const hashedPassword = await this.cryptoService.hash(dto.password);
+    const hashedOtp = await this.cryptoService.hash(otp);
+    try {
+      const result = await this.prisma.$transaction(async (tx) => {
+        const newUser = await tx.user.create({
+          data: {
+            email,
+            name,
+            password: hashedPassword,
+          },
+          select: { id: true, email: true },
+        });
 
-    //create the user and store data in the user table in the DB
-    const newUser = await this.prisma.user.create({
-      data: {
-        email: dto.email.toLowerCase(),
-        name: dto.name,
-        password: hashedPassword,
-      },
-      select: { id: true },
-    });
+        await tx.emailVerification.create({
+          data: {
+            email: newUser.email,
+            hashedOtp,
+            expiresAt: new Date(expiresAt),
+          },
+        });
+        const token = await this.createTokensAndSession(
+          newUser.id,
+          device,
+          ipAddress,
+          tx,
+        );
 
-    return this.createTokensAndSession(newUser.id, device, ipAddress);
+        return { newUser, token };
+      });
+
+      //send email
+      await this.emailService.accountVerification(result.newUser.email, otp);
+
+      return {
+        accessToken: result.token.accessToken,
+        refreshToken: result.token.refreshToken,
+        refreshTokenTtlMs: result.token.refreshTokenTtlMs,
+      };
+    } catch (error) {
+      if (
+        error instanceof Prisma.PrismaClientKnownRequestError &&
+        error.code === 'P2002'
+      ) {
+        throw new ConflictException('Account already exists');
+      }
+      throw error;
+    }
   }
 
   async loginUser(dto: LoginDto, device: string, ipAddress: string) {
@@ -148,13 +186,6 @@ export class AuthService {
     return this.createTokensAndSession(existingUser.id, device, ipAddress);
   }
 
-  private async revokeAllUserSessions(userId: string) {
-    await this.prisma.session.updateMany({
-      where: { userId, isRevoked: false },
-      data: { isRevoked: true, revokedAt: new Date() },
-    });
-  }
-
   async rotateRefreshToken(
     incomingRefreshToken: string,
     device: string,
@@ -164,62 +195,96 @@ export class AuthService {
       throw new BadRequestException('Provide refresh token');
     }
 
+    const now = new Date();
+
     // Hash the incoming refresh token so it can be compared with the stored hash in the DB
     const hashedIncomingToken =
       this.cryptoService.hashValue(incomingRefreshToken);
 
-    // Find the session that belongs to this refresh token
-    const existingSession = await this.prisma.session.findUnique({
-      where: { token: hashedIncomingToken },
-      select: { userId: true, isRevoked: true, expiresAt: true },
-    });
+    try {
+      const result = await this.prisma.$transaction(async (tx) => {
+        const revokeResult = await tx.session.updateMany({
+          where: {
+            token: hashedIncomingToken,
+            isRevoked: false,
+            expiresAt: { gt: new Date() },
+          },
+          data: {
+            isRevoked: true,
+            revokedAt: new Date(),
+          },
+        });
 
-    if (!existingSession) {
-      throw new NotFoundException('Session not found');
-    }
+        if (revokeResult.count !== 1) {
+          const reusedSession = await tx.session.findUnique({
+            where: { token: hashedIncomingToken },
+            select: {
+              userId: true,
+              isRevoked: true,
+            },
+          });
 
-    // If the token is already revoked or expired, this could be a reuse attack revoke all user sessions that are still active
-    if (existingSession.isRevoked || existingSession.expiresAt < new Date()) {
-      await this.revokeAllUserSessions(existingSession.userId);
-      throw new UnauthorizedException('Refresh token is invalid or expired');
-    }
+          if (reusedSession?.isRevoked) {
+            await tx.session.updateMany({
+              where: {
+                userId: reusedSession.userId,
+                isRevoked: false,
+              },
+              data: {
+                isRevoked: true,
+                revokedAt: now,
+              },
+            });
+          }
 
-    // Generate a new access and refresh token for the user
-    const {
-      accessToken,
-      refreshToken: newRefreshToken,
-      expiresAt: newExpiresAt,
-      refreshTokenTtlMs,
-    } = this.createAuthTokens(existingSession.userId);
+          throw new UnauthorizedException('Invalid refresh token');
+        }
 
-    const hashedNewRefreshToken = this.cryptoService.hashValue(newRefreshToken);
+        const oldSession = await tx.session.findUnique({
+          where: { token: hashedIncomingToken },
+          select: {
+            userId: true,
+          },
+        });
 
-    // In one transaction — create the new session and revoke the old one
-    await this.prisma.$transaction(async (tx) => {
-      await this.createSession(
-        {
-          userId: existingSession.userId,
-          token: hashedNewRefreshToken,
-          device,
-          ipAddress,
-          expiresAt: newExpiresAt,
-        },
-        tx,
-      );
-      await tx.session.update({
-        where: { token: hashedIncomingToken },
-        data: {
-          isRevoked: true,
-          revokedAt: new Date(),
-        },
+        if (!oldSession) {
+          throw new UnauthorizedException('Invalid refresh token');
+        }
+
+        const { accessToken, refreshToken, expiresAt, refreshTokenTtlMs } =
+          this.createAuthTokens(oldSession.userId);
+
+        const hashedNewRefreshToken =
+          this.cryptoService.hashValue(refreshToken);
+
+        // create a new session
+        await tx.session.create({
+          data: {
+            userId: oldSession.userId,
+            token: hashedNewRefreshToken,
+            device,
+            ipAddress,
+            expiresAt,
+          },
+        });
+
+        return {
+          accessToken,
+          refreshToken,
+          refreshTokenTtlMs,
+        };
       });
-    });
 
-    return {
-      accessToken,
-      refreshToken: newRefreshToken,
-      refreshTokenTtlMs,
-    };
+      return result;
+    } catch (error) {
+      if (
+        error instanceof UnauthorizedException ||
+        error instanceof BadRequestException
+      ) {
+        throw error;
+      }
+      throw new InternalServerErrorException('Unable to rotate refresh token');
+    }
   }
 
   async signOut(incomingRefreshToken: string) {
